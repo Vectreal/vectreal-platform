2f520c6f2e052b40d75b575224a23b6dec51a680
Schlomoh <post@moritzbecker.de>
2025-09-07 03:08:49 +0200

    feat: enhance project configuration with ESLint, Vite, and Drizzle setup

--- apps/vectreal-platform/vite.config.ts
+++ apps/vectreal-platform/vite.config.ts
@@ -4,43 +4,51 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import devtoolsJson from 'vite-plugin-devtools-json'
 
-export default defineConfig(() => ({
-	root: __dirname,
-	cacheDir: '../../node_modules/.vite/apps/vectreal-platform',
-	server: {
-		port: 4200,
-		host: 'localhost'
-	},
-	preview: {
-		port: 4300,
-		host: 'localhost'
-	},
-	plugins: [
-		tailwindcss(),
-		!process.env.VITEST && reactRouter(),
-		nxViteTsPaths(),
-		nxCopyAssetsPlugin(['*.md'])
-	],
-	// worker: {
-	//  plugins: [ nxViteTsPaths() ],
-	// },
-	build: {
-		emptyOutDir: true,
-		reportCompressedSize: true,
-		commonjsOptions: {
-			transformMixedEsModules: true
-		}
-	},
-	test: {
-		watch: false,
-		globals: true,
-		environment: 'jsdom',
-		include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-		reporters: ['default'],
-		coverage: {
-			reportsDirectory: '../../coverage/apps/vectreal-platform',
-			provider: 'v8' as const
+export default defineConfig(({ mode, isSsrBuild, command }) => {
+	return {
+		root: __dirname,
+		cacheDir: '../../node_modules/.vite/apps/vectreal-platform',
+		server: {
+			port: 4200,
+			host: 'localhost'
+		},
+		preview: {
+			port: 4300,
+			host: 'localhost'
+		},
+		plugins: [
+			tailwindcss(),
+			nxViteTsPaths(),
+			nxCopyAssetsPlugin(['*.md']),
+			devtoolsJson({
+				projectRoot: __dirname
+			}),
+			!process.env.VITEST && reactRouter()
+		],
+		// worker: {
+		//  plugins: [ nxViteTsPaths() ],
+		// },
+		// Removed 'ssr.external' because it's incompatible with Cloudflare Vite plugin
+		// Externals are now only handled in rollupOptions.external
+		build: {
+			emptyOutDir: true,
+			reportCompressedSize: true,
+			commonjsOptions: {
+				transformMixedEsModules: true
+			}
+		},
+		test: {
+			watch: false,
+			globals: true,
+			environment: 'jsdom',
+			include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+			reporters: ['default'],
+			coverage: {
+				reportsDirectory: '../../coverage/apps/vectreal-platform',
+				provider: 'v8' as const
+			}
 		}
 	}
-}))
+})
