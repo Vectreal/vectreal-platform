f8bfade5a95e014a8eab0a09a8d2230160304d77
Schlomoh <post@moritzbecker.de>
2025-09-07 03:07:07 +0200

    refactor: remove official-website app

--- apps/vectreal-platform/app/routes/dashboard-page/dashboard-page.tsx
+++ apps/vectreal-platform/app/routes/dashboard-page/dashboard-page.tsx
@@ -1,3 +1,11 @@
+import { userService } from '../../lib/services/user.service'
+
+import { Route } from './+types/dashboard-page'
+
+export async function loader({ request }: Route.LoaderArgs) {
+	return null
+}
+
 const DashboardPage = () => {
 	return <div></div>
 }
