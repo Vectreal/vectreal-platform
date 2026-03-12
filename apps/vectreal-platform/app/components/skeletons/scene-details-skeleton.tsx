2988b8fd8c11a0c5b6cf63aa95b177ab1866bffb
Schlomoh <post@moritzbecker.de>
2026-03-12 21:25:51 +0100

    feat: adjust scene details skeleton layout for improved responsiveness

--- apps/vectreal-platform/app/components/skeletons/scene-details-skeleton.tsx
+++ apps/vectreal-platform/app/components/skeletons/scene-details-skeleton.tsx
@@ -3,12 +3,12 @@ import { Skeleton } from '@shared/components'
 export const SceneDetailsSkeleton = () => {
 	return (
 		<div className="mt-12 grid h-full gap-6 p-6 md:grid-cols-[3fr_1fr]">
-			<div className="grid w-full grid-rows-[5fr_2fr] gap-6">
+			<div className="grid w-full grid-rows-[3fr_2fr] gap-6 lg:grid-rows-[5fr_2fr]">
 				<Skeleton className="animate-pulse!" />
 				<Skeleton className="animate-pulse!" />
 			</div>
 
-			<Skeleton className="animate-pulse!" />
+			<Skeleton className="hidden animate-pulse! lg:block" />
 		</div>
 	)
 }
