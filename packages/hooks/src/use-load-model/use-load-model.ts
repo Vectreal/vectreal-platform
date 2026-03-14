c6745e80cede67c99f64ce32f2659edfabe487db
Schlomoh <post@moritzbecker.de>
2026-03-14 03:43:20 +0100

    chore: remove unused import of useEffect from use-load-model

--- packages/hooks/src/use-load-model/use-load-model.ts
+++ packages/hooks/src/use-load-model/use-load-model.ts
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 import { ModelFileTypes, ModelLoader } from '@vctrl/core/model-loader'
-import { useCallback, useEffect, useMemo, useReducer, useRef } from 'react'
+import { useCallback, useMemo, useReducer, useRef } from 'react'
 
 import eventSystem from './event-system'
 import reducer, { initialState } from './state'
