942eb08f92047e7a7f77fe68a9b5f5db060f056c
Schlomoh <post@moritzbecker.de>
2026-04-12 22:05:45 +0200

    fix: adjust margin in SceneDetailsSkeleton for improved layout consistency

--- apps/vectreal-platform/app/components/skeletons/scene-details-skeleton.tsx
+++ apps/vectreal-platform/app/components/skeletons/scene-details-skeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@shared/components'
 
 export const SceneDetailsSkeleton = () => {
 	return (
-		<div className="mt-12 grid h-full gap-6 p-6 xl:grid-cols-[3fr_1fr]">
+		<div className="grid h-full gap-6 px-6 xl:grid-cols-[3fr_1fr]">
 			<div className="grid w-full grid-rows-[3fr_2fr] gap-6 lg:grid-rows-[5fr_2fr]">
 				<Skeleton className="animate-pulse!" />
 				<Skeleton className="animate-pulse!" />
